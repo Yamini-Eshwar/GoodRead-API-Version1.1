0927843d39e64d3cfe44d7a403c8043bac66cb91
yamini <yaminieshwar@gmail.com>
2023-12-23 07:18:59 +0000

    adds book onto goodreads.db database

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 
 const app = express()
+app.use(express.json())
+
 const dbPath = path.join(__dirname, 'goodreads.db')
 
 let db = null
@@ -50,5 +52,17 @@ app.get('/books/:bookId/', async (request, response) => {
 
   const book = await db.get(getBooksQuery)
 
-  response.send('Hello World!!')
+  response.send(book)
+})
+
+//ADD Book API
+app.post('/books/', async (request, response) => {
+  const bookDetails = request.body
+  const {title, authorId, rating, description, pages, price} = bookDetails
+  const addBookQuery = `INSERT INTO book (title, author_Id, rating,description,pages,price)
+                             VALUES ('${title}',${authorId},${rating},'${description}',${pages},${price});`
+  const dbResponse = await db.run(addBookQuery)
+  const bookId = dbResponse.lastID
+  console.log(bookId)
+  response.send({bookId: bookId})
 })
